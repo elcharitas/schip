b4025e923be8b49f61827fa977dd85523298ccc6
elcharitas <jonathanirhodia@gmail.com>
2020-12-29 21:37:29 +0100

    Add basic wrapper setup

---
+++ README.md
@@ -0,0 +1,24 @@
+<div align="center">
+
+# Schippo CLI
+
+CLI Utility to use custom wrappers
+
+[![Build Status](https://travis-ci.org/<USERNAME>/<REPO>.svg?branch=master)](https://travis-ci.org/<USERNAME>/<REPO>)
+[![Licence](https://img.shields.io/github/license/<USERNAME>/<PROJECT_NAME>)](LICENSE)
+[![NPM](https://img.shields.io/npm/v/<PROJECT_NAME>)](https://www.npmjs.com/package/<PROJECT_NAME>)
+
+<img src="" alt="">
+
+</div>
+
+## What Can it Do?
+
+## Install
+
+## Example
+
+## Development
+
+## Contributing
+
---
+++ main.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+const { existsSync, readFileSync } = require("fs")
+const { resolve } = require("path")
+const currentDir = process.cwd()
+const pkgDir = resolve(currentDir, "./package.json")
+
+if (existsSync(pkgDir)) {
+    const package = JSON.parse(readFileSync(pkgDir))
+    try {
+        const pkg = package?.bin[Object.keys(package.bin)[0]] || "./bin.js"
+        require(resolve(currentDir, pkg))
+    }
+    catch (e) {
+        console.log("Schippo Requires a binary to be set")
+    }
+} else {
+    console.log("Schippo can only be used from root of npm package")
+}
